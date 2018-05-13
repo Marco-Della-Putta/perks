ef2468543ce6b41a8e150ff651c6df1d728462b3
Marco Della Putta <39233605+Marco-Della-Putta@users.noreply.github.com>
2018-05-13 15:12:40 +0200

    Add files via upload

---
+++ perk_mdplib/sample/SubLibrary/subprocess.py
@@ -0,0 +1,14 @@
+# You can add SubProcess to the perk_mdplib
+# Good Luck.
+
+class Example:
+    """
+    Example of docstring.
+    """
+    def __init__(self):
+        self.parameter = "something"
+
+    def SomeFunct(self):
+        return self.parameter
+
+## START SUBPROCESS DECLARATION ##
