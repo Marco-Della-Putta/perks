453445a3158c620b7af18a3ec5fedb00040a88bc
Marco Della Putta <39233605+Marco-Della-Putta@users.noreply.github.com>
2018-05-24 16:52:35 +0200

    Add files via upload

--- perks_package/setup/setup.py
+++ perks_package/setup/setup.py
@@ -13,7 +13,7 @@ Good Luck!
 """
 
 setup(name='perks',
-      version='1.25.3',
+      version='1.25.4',
       author='Marco Della Putta',
       author_email='marcodp183@gmail.com',
       url='https://github.com/Marco-Della-Putta/perks',
